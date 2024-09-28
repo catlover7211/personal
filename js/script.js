bc704ecc8eb0226ad20982f71774cccca0c900a1
catlover <bm963852@gmail.com>
2024-09-28 23:05:00 +0800

    font

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed('#typed', {
+        strings: ["Catlover."],
+        typeSpeed: 100, // 打字速度
+        backSpeed: 50, // 刪除速度
+        backDelay: 1000, // 刪除前的等待時間
+        loop: false, // 是否循環
+        showCursor: false,
+        cursorChar: '|',
+    });
+});
