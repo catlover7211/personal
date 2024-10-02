6d3b9f871db6c2df67fbf6c00d185ac40b05a578
catlover <bm963852@gmail.com>
2024-10-02 11:29:14 +0800

    trans

--- trans.sh
+++ trans.sh
@@ -4,12 +4,13 @@
 USER="kali"                 # 遠端使用者名稱
 HOST="10.16.20.28"          # 遠端主機 IP 或域名
 REMOTE_DIR="webserver/web"  # 遠端目標目錄
+PASSWORD="kali"           # 遠端主機密碼
 
 # 本地目錄
 LOCAL_DIR="./*"
 
-# 執行 scp 命令
-scp -r $LOCAL_DIR $USER@$HOST:$REMOTE_DIR
+# 使用 sshpass 自動輸入密碼執行 scp 命令
+sshpass -p "$PASSWORD" scp -r $LOCAL_DIR $USER@$HOST:$REMOTE_DIR
 
 # 判斷是否成功
 if [ $? -eq 0 ]; then
